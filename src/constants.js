13789ca2014465cf6a56557cbf149439d377dddc
shucheng <gu900327@hotmail.com>
2020-07-23 14:30:25 -0500

    log in

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const API_ROOT = 'https://around-179500.uc.r.appspot.com';
+export const TOKEN_KEY = 'TOKEN_KEY';
+export const GEO_OPTIONS = {
+    enableHighAccuracy: true,
+    maximumAge        : 300000,
+    timeout           : 27000,
+};
